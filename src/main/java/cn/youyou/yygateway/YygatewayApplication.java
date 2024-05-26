19f862ada9cc336a8e6a9052fbad97606b07f28c
XianReallyHot-ZZH <741788769@qq.com>
2024-05-26 16:51:03 +0800

    init:项目初始化

---
+++ src/main/java/cn/youyou/yygateway/YygatewayApplication.java
@@ -0,0 +1,13 @@
+package cn.youyou.yygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YygatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YygatewayApplication.class, args);
+    }
+
+}
